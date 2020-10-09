75431b1682fd38d7b2c9245b79f346a6056a6d96
Aaron C Wacker <30595158+AaronCWacker@users.noreply.github.com>
2020-10-08 21:22:38 -0500

    🦈 Great White Shark

---
+++ nlpSpeechProxy/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ChromeSpeechProxy
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Server.SetAppName(AppDomain.CurrentDomain.FriendlyName);
+
+            if (args.Length > 0)
+            {
+                string strPort = args[0];
+                int port;
+                if (int.TryParse(strPort, out port))
+                {
+                    Server.SetProxyPort(port);
+                }
+            }
+            string[] commandArgs = Environment.GetCommandLineArgs();
+            if (commandArgs.Length > 0)
+            {
+                FileInfo fi = new FileInfo(commandArgs[0]);
+                string installDir = fi.DirectoryName;
+                Server.SetInstallDirectory(installDir);
+            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
